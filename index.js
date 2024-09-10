a6d1ded990a8c58cbdbf9a9cc961b88bc27d1502
flapberry <flapberrywhistle@gmail.com>
2024-09-10 10:29:06 +0530

    v6

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 console.log('🙎‍♀️', 'hello world');
-<<<<<<< HEAD
 console.log('😻', 'hello');
 console.log('👩‍❤️‍💋‍👩', 'hello 3');
-=======
 console.log('😻', 'hello');
->>>>>>> ba5c615fa3434a3d3dbf76a10f14b32302746b89
 
 function run() {
 	
